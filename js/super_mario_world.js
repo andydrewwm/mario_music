fbd08111e34e4eddcfaebd07c377a74ae1a11244
awilkmil <awilkmil@umich.edu>
2023-12-03 02:01:26 -0500

    SMW complete

--- js/super_mario_world.js
+++ js/super_mario_world.js
@@ -1,47 +1,104 @@
-const overworldTheme = new Audio("./audio/smw_overworld_theme.oga");
-const overworldYoshiTheme = new Audio("./audio/smw_overworld_yoshi_theme.oga");
-const rideYoshiSound = new Audio("./audio/smw_riding_yoshi.wav");
-const loseYoshiSound = new Audio("./audio/smw_yoshi_runs_away.wav");
+const regTracks = [
+    new Audio("./audio/smw_overworld.mp3"),
+    new Audio("./audio/smw_athletic.mp3"),
+    new Audio("./audio/smw_underwater.mp3"),
+    new Audio("./audio/smw_underground.mp3"),
+]
+const yoshiTracks = [
+    new Audio("./audio/smw_overworld_yoshi.mp3"),
+    new Audio("./audio/smw_athletic_yoshi.mp3"),
+    new Audio("./audio/smw_underwater_yoshi.mp3"),
+    new Audio("./audio/smw_underground_yoshi.mp3"),
+]
 
-async function toggleOverworld(element) {
-    const eltClass = element.className;
+const onYoshiSfx = new Audio("./audio/smw_riding_yoshi.wav");
+const offYoshiSfx = new Audio("./audio/smw_yoshi_runs_away.wav");
 
-    if (eltClass === "mario") {
-        if (!overworldYoshiTheme.muted && !overworldTheme.paused) loseYoshiSound.play();
-        overworldTheme.muted = false;
-        overworldYoshiTheme.muted = true;
+const HIGHLIGHT = "rgba(207, 97, 46, 0.5)";
 
-        const yoshiButton = document.getElementsByClassName("yoshi")[0];
-        yoshiButton.style.backgroundColor = "";
-        element.style.backgroundColor = "rgba(207, 97, 46, 0.5)";
+async function toggleMusic(elt) {
+    const is_mario = elt.className === "mario";
+    const level = elt.parentElement.id
+    let levelIndex;
+    let animateClass = `animate-${level}`;
 
-    } else if (eltClass === "yoshi") {
-        if (overworldYoshiTheme.muted || overworldYoshiTheme.paused) rideYoshiSound.play();
-        overworldYoshiTheme.muted = false;
-        overworldTheme.muted = true;
-
-        const marioButton = document.getElementsByClassName("mario")[0];
-        marioButton.style.backgroundColor = "";
-        element.style.backgroundColor = "rgba(207, 97, 46, 0.5)";
+    switch (level) {
+        case "overworld":
+            levelIndex = 0;
+            break;
+        case "athletic":
+            levelIndex = 1;
+            break;
+        case "underwater":
+            levelIndex = 2;
+            break;
+        case "underground":
+            levelIndex = 3;
+            break;
+        default:
+            break;
     }
 
-    if (overworldTheme.currentTime == 0 || overworldTheme.currentTime == overworldTheme.duration) {
-        document.getElementById("overworld").className = "animateLevel";
+    const regTrack = regTracks[levelIndex];
+    const yoshiTrack = yoshiTracks[levelIndex];
+
+    if (is_mario) {
+        if (!yoshiTrack.muted && !regTrack.paused) offYoshiSfx.play();
+        regTrack.muted = false;
+        yoshiTrack.muted = true;
+
+        document.getElementsByClassName("yoshi")[levelIndex].style.backgroundColor = "";
+        elt.style.backgroundColor = HIGHLIGHT;
+    } else {
+        if (yoshiTrack.muted || yoshiTrack.paused) onYoshiSfx.play();
+        yoshiTrack.muted = false;
+        regTrack.muted = true;
+
+        document.getElementsByClassName("mario")[levelIndex].style.backgroundColor = "";
+        elt.style.backgroundColor = HIGHLIGHT;
     }
 
-    overworldTheme.play();
-    overworldYoshiTheme.play();
+    if (regTrack.currentTime == 0 || regTrack.currentTime == regTrack.duration) {
+        document.getElementById(level).className = animateClass;
+    }
 
-    overworldTheme.onpause = () => {
-        document.getElementById("overworld").className = "";
+    regTrack.play();
+    yoshiTrack.play();
+
+    regTrack.onpause = () => {
+        document.getElementById(level).className = "";
         element.style.backgroundColor = "";
     }
 }
 
-function stopMusic() {
-    overworldTheme.pause();
-    overworldYoshiTheme.pause();
+function stopMusic(elt) {
+    const level = elt.parentElement.id
+    let levelIndex;
+    switch (level) {
+        case "overworld":
+            levelIndex = 0;
+            break;
+        case "athletic":
+            levelIndex = 1;
+            break;
+        case "underwater":
+            levelIndex = 2;
+            break;
+        case "underground":
+            levelIndex = 3;
+            break;
+        default:
+            break;
+    }
+    const regTrack = regTracks[levelIndex];
+    const yoshiTrack = yoshiTracks[levelIndex];
 
-    overworldTheme.currentTime = 0;
-    overworldYoshiTheme.currentTime = 0;
+    regTrack.pause();
+    yoshiTrack.pause();
+
+    regTrack.currentTime = 0;
+    yoshiTrack.currentTime = 0;
+
+    document.getElementsByClassName("mario")[levelIndex].style.backgroundColor = "";
+    document.getElementsByClassName("yoshi")[levelIndex].style.backgroundColor = "";
 }
